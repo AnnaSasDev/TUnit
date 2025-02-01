b6efd588666f635581f95167fdde470164eb33ca
Tom Longhurst <30480171+thomhurst@users.noreply.github.com>
2025-02-01 10:49:20 +0000

    WIP

---
+++ TUnit.FsCheck/FsCheckAttribute.cs
@@ -0,0 +1,55 @@
+﻿using FsCheck;
+using TUnit.Core;
+using TUnit.Core.Enums;
+using TUnit.Core.Interfaces;
+using TUnit.Engine.Extensions;
+
+namespace TUnit.FsCheck;
+
+public abstract class FsCheckAttribute<T> : DataSourceGeneratorAttribute<T>, ITestEndEventReceiver
+{
+    private DataGeneratorMetadata _dataGeneratorMetadata;
+    
+    public Arbitrary<T>? Arbitrary => field ??= CreateGenerator(_dataGeneratorMetadata);
+
+    public override IEnumerable<Func<T>> GenerateDataSources(DataGeneratorMetadata dataGeneratorMetadata)
+    {
+        _dataGeneratorMetadata = dataGeneratorMetadata;
+        
+        foreach (var data in Arbitrary!.Generator.Sample(50, SampleSize))
+        {
+            yield return () => data;
+        }
+    }
+    
+    protected abstract Arbitrary<T> CreateGenerator(DataGeneratorMetadata dataGeneratorMetadata);
+    protected virtual int SampleSize => 100;
+    
+    public async ValueTask OnTestEnd(TestContext testContext)
+    {
+        if (testContext.Result?.Status is not Status.Failed)
+        {
+            return;
+        }
+        
+        var args = testContext.TestDetails.TestMethodArguments ?? [];
+        
+        var t = args.OfType<T>().First();
+        
+        var shrinkValues = Arbitrary!.Shrinker(t).ToArray();
+
+        if (shrinkValues.Any())
+        {
+            testContext.SuppressReportingResult();
+        }
+        
+        foreach (var shrinkT in shrinkValues)
+        {
+#pragma warning disable WIP
+            await testContext.ReregisterTestWithArguments([shrinkT]);;
+#pragma warning restore WIP
+        }
+    }
+
+    public int Order => 0;
+}
---
+++ TUnit.TestProject/FsCheckTests.cs
@@ -0,0 +1,28 @@
+﻿using FsCheck;
+using TUnit.Assertions;
+using TUnit.Assertions.Extensions;
+using TUnit.FsCheck;
+
+namespace TUnit.TestProject;
+
+public class FsCheckTests
+{
+    [Test]
+    [Data]
+    public async Task FsCheckTest(string data)
+    {
+        await Assert.That(data).IsNotNull()
+            .And.DoesNotContain('A')
+            .And.DoesNotContain('a');
+    }
+    
+    public class DataAttribute : FsCheckAttribute<string>
+    {
+        protected override Arbitrary<string> CreateGenerator(DataGeneratorMetadata dataGeneratorMetadata)
+        {
+            return Arb.Default.String();
+        }
+        
+        protected override int SampleSize => 5;
+    }
+}
